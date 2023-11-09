1cc31195c324c2008675f0dc6920f18310c16785
MihaiM36 <133246280+MihaiM36@users.noreply.github.com>
2023-11-09 09:05:56 +0200

    Main

---
+++ src/test/java/ro/uvt/info/splab2023/SpLabApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.uvt.info.splab2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
