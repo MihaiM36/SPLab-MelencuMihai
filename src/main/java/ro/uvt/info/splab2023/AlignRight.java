8fb18f692a4b0943afcbffe2dde279fa341aaa46
MihaiM36 <133246280+MihaiM36@users.noreply.github.com>
2023-11-23 10:32:08 +0200

    Delete src/main/java/ro/uvt/info/splab2023/AlignRight.java

--- src/main/java/ro/uvt/info/splab2023/AlignRight.java
+++
@@ -1,8 +0,0 @@
-package ro.uvt.info.splab2023;
-
-public class AlignRight implements AlignStrategy {
-    public String align(String content) {
-        // Implement right alignment logic here
-        return "Right: " + content;
-    }
-}
